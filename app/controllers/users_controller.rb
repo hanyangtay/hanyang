09a1303f4f097c2702cced251b0de30c99330129
Han Yang Tay <htay@wesleyan.edu>
2017-01-12 01:41:29 +0000

    Fixed document ready bugs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update, :destroy, 
-                                        :following, :followers]
+  before_action :logged_in_user, only: [:edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
   before_action :not_guest, only: :update

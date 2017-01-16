0be2ebf0d3f04117248910c7013ff383c00654e4
Han Yang Tay <htay@wesleyan.edu>
2017-01-16 08:36:43 +0000

    Undo scope

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170114184133) do
+ActiveRecord::Schema.define(version: 20170115052124) do
 
   create_table "likes", force: :cascade do |t|
     t.integer  "liked_post_id"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 20170114184133) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "online_users", force: :cascade do |t|
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_online_users_on_user_id"
   end
 
   create_table "relationships", force: :cascade do |t|
